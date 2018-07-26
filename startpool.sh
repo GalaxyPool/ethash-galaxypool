1591dc94510ac0eeae41cbaf842e35bb62ba0c88
minerpoolcf <41727706+minerpoolcf@users.noreply.github.com>
2018-07-27 00:50:13 +0700

    update

--- startpool.sh
+++ startpool.sh
@@ -2,20 +2,20 @@
 
 #pool2b:
 echo starting 2G diff pool
-screen -S pool2b ./build/bin/ethash-minerpool pool2b.json
+screen -S pool2b ./build/bin/ethash-minerpool ~/ethash-minerpool/pool2b.json
 
 #pool9b:
 echo starting 9G diff pool
-screen -S pool9b ./build/bin/ethash-minerpool pool9b.json
+screen -S pool9b ./build/bin/ethash-minerpool ~/ethash-minerpool/pool9b.json
 
 #api:
 echo starting API
-screen -S api ./build/bin/ethash-minerpool api.json
+screen -S api ./build/bin/ethash-minerpool ~/ethash-minerpool/api.json
 
 #unlocker:
 echo starting unlocker
-screen -S unlocker ./build/bin/ethash-minerpool unlocker.json
+screen -S unlocker ./build/bin/ethash-minerpool ~/ethash-minerpool/unlocker.json
 
 #payout:
 echo starting payout
-screen -S payout ./build/bin/ethash-minerpool payout.json
+screen -S payout ./build/bin/ethash-minerpool ~/ethash-minerpool/payout.json
