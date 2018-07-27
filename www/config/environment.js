93bee9709a891fc508bd0352bd6239d7f8a3d269
minerpoolcf <41727706+minerpoolcf@users.noreply.github.com>
2018-07-27 18:16:59 +0700

    update

--- www/config/environment.js
+++ www/config/environment.js
@@ -27,7 +27,7 @@ module.exports = function(environment) {
 
       // Fee and payout details
       PoolFee: '1%',
-      PayoutThreshold: '0.5 Ether',
+      PayoutThreshold: '1 AURA',
 
       // For network hashrate (change for your favourite fork)
       BlockTime: 14.4
