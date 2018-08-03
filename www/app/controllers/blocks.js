cafb776760207a9e7807e7b44057b347752ba59f
minerpoolcf <41727706+minerpoolcf@users.noreply.github.com>
2018-08-03 19:56:13 +0700

    Revert "update earperday"
    This reverts commit ef8d5672e992cac8df848da99f95be492d6b8252.

--- www/app/controllers/blocks.js
+++ www/app/controllers/blocks.js
@@ -2,6 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   applicationController: Ember.inject.controller('application'),
-  stats: Ember.computed.reads('applicationController'),
   config: Ember.computed.reads('applicationController.config')
 });
--- www/app/helpers/worker-earnperday.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import config from '../config/environment';
-
-export default Ember.Helper.extend({
-  compute(hashrates) {
-    return 24 * 60 * 60 * (hashrates[0] / hashrates[1]) * 9 * 0.95;
-  }
-});
