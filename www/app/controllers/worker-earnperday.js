7d8502be65b18986d74e86df8a1f85e9aa546aa0
minerpoolcf <41727706+minerpoolcf@users.noreply.github.com>
2018-08-03 19:56:42 +0700

    Revert "update earnperday"
    This reverts commit 0c25525172f4f35d6f40fec0b2bc6e554b7e4bb4.

--- www/app/controllers/worker-earnperday.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-import config from '../config/environment';
-export default Ember.Helper.extend({
-  compute(hashrates) {
-    return 24 * 60 * 60 / config.APP.BlockTime * (hashrates[0] / hashrates[1]) * config.APP.BlockReward;
-  }
-});
